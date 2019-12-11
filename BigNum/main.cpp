95484de6b5c11e684815975fba073c4ecda7c9a3
ShacharAvni <shachar.avni@printbear.net>
2019-12-11 15:22:07 -0500

    Adds unit test verification

--- BigNum/main.cpp
+++ BigNum/main.cpp
@@ -3,76 +3,112 @@
 #include <iostream>
 #include <limits>
 
-void singleAdditionUnitTest(const std::string& a, const std::string& b)
+unsigned int numPassed = 0;
+unsigned int numFailed = 0;
+
+template <typename resultType>
+void printResult(const resultType& actualResult)
 {
-    std::cout << a << " + " << b << " is " << (BigNum(a) + BigNum(b)).display() << std::endl;
+    std::cout << actualResult;
+}
+
+template <>
+void printResult(const bool& actualResult)
+{
+    std::cout << std::boolalpha << actualResult;
+}
+
+template <typename resultType>
+void runUnitTest(const std::string& a, const std::string& b, const std::string& operation, const resultType& actualResult, const resultType& expectedResult)
+{
+    std::cout << a << operation << b << " is ";
+    printResult(actualResult);
+    std::cout << " : ";
+
+    if (actualResult == expectedResult)
+    {
+        std::cout << "PASS";
+        ++numPassed;
+    }
+    else
+    {
+        std::cout << "FAIL";
+        ++numFailed;
+    }
+
+    std::cout << std::endl;
+}
+
+void singleAdditionUnitTest(const std::string& a, const std::string& b, const std::string& expectedResult)
+{
+    runUnitTest(a, b, " + ", (BigNum(a) + BigNum(b)).display(), expectedResult);
 }
 
 void additionUnitTests()
 {
-    singleAdditionUnitTest("0", "0"); // should be 0
-    singleAdditionUnitTest("1", "0"); // should be 1
-    singleAdditionUnitTest("1", "1"); // should be 2
-    singleAdditionUnitTest("1234", "1234"); // should be 2468
-    singleAdditionUnitTest("97654321", "812345512"); // should be 909999833
+    singleAdditionUnitTest("0", "0", "0");
+    singleAdditionUnitTest("1", "0", "1");
+    singleAdditionUnitTest("1", "1", "2");
+    singleAdditionUnitTest("1234", "1234", "2468");
+    singleAdditionUnitTest("97654321", "812345512", "909999833");
 
-    singleAdditionUnitTest("1", "-1"); // should be 0
-    singleAdditionUnitTest("-1234", "-3456"); // should be -4690
-    singleAdditionUnitTest("-3456", "1234"); // -2222
+    singleAdditionUnitTest("1", "-1", "0");
+    singleAdditionUnitTest("-1234", "-3456", "-4690");
+    singleAdditionUnitTest("-3456", "1234", "-2222");
 }
 
-void singleSubtractionUnitTest(const std::string& a, const std::string& b)
+void singleSubtractionUnitTest(const std::string& a, const std::string& b, const std::string& expectedResult)
 {
-    std::cout << a << " - " << b << " is " << (BigNum(a) - BigNum(b)).display() << std::endl;
+    runUnitTest(a, b, " - ", (BigNum(a) - BigNum(b)).display(), expectedResult);
 }
 
 void subtractionUnitTests()
 {
-    singleSubtractionUnitTest("0", "0"); // should be 0
-    singleSubtractionUnitTest("1", "0"); // should be 1
-    singleSubtractionUnitTest("1", "1"); // should be 0
-    singleSubtractionUnitTest("2468", "1234"); // should be 1234
-    singleSubtractionUnitTest("97654321", "812345512"); // should be -714691191
+    singleSubtractionUnitTest("0", "0", "0");
+    singleSubtractionUnitTest("1", "0", "1");
+    singleSubtractionUnitTest("1", "1", "0");
+    singleSubtractionUnitTest("2468", "1234", "1234");
+    singleSubtractionUnitTest("97654321", "812345512", "-714691191");
 
-    singleSubtractionUnitTest("1", "-1"); // should be 2
-    singleSubtractionUnitTest("-1234", "-3456"); // should be 2222
-    singleSubtractionUnitTest("-3456", "1234"); // should be -4690
+    singleSubtractionUnitTest("1", "-1", "2");
+    singleSubtractionUnitTest("-1234", "-3456", "2222");
+    singleSubtractionUnitTest("-3456", "1234", "-4690");
 }
 
-void singleLessThanUnitTest(const std::string& a, const std::string& b)
+void singleLessThanUnitTest(const std::string& a, const std::string& b, bool expectedResult)
 {
-    std::cout << a << " < " << b << " is " << std::boolalpha << (BigNum(a) < BigNum(b)) << std::endl;
+    runUnitTest(a, b, " < ", (BigNum(a) < BigNum(b)), expectedResult);
 }
 
 void lessThanUnitTests()
 {
-    singleLessThanUnitTest("0", "0"); // should be false
-    singleLessThanUnitTest("1", "0"); // should be false
-    singleLessThanUnitTest("0", "1"); // should be true
-    singleLessThanUnitTest("1234", "123"); // should be false
-    singleLessThanUnitTest("45681565515", "45681566515"); // should be true
+    singleLessThanUnitTest("0", "0", false);
+    singleLessThanUnitTest("1", "0", false);
+    singleLessThanUnitTest("0", "1", true);
+    singleLessThanUnitTest("1234", "123", false);
+    singleLessThanUnitTest("45681565515", "45681566515", true);
 
-    singleLessThanUnitTest("1234", "-1234"); // should be false
-    singleLessThanUnitTest("-3456", "1234"); // should be true
-    singleLessThanUnitTest("-1234", "-3456"); // should be false
+    singleLessThanUnitTest("1234", "-1234", false);
+    singleLessThanUnitTest("-3456", "1234", true);
+    singleLessThanUnitTest("-1234", "-3456", false);
 }
 
-void singleGreaterThanUnitTest(const std::string& a, const std::string& b)
+void singleGreaterThanUnitTest(const std::string& a, const std::string& b, bool expectedResult)
 {
-    std::cout << a << " > " << b << " is " << std::boolalpha << (BigNum(a) > BigNum(b)) << std::endl;
+    runUnitTest(a, b, " > ", (BigNum(a) > BigNum(b)), expectedResult);
 }
 
 void greaterThanUnitTests()
 {
-    singleGreaterThanUnitTest("0", "0"); // should be false
-    singleGreaterThanUnitTest("1", "0"); // should be true
-    singleGreaterThanUnitTest("0", "1"); // should be false
-    singleGreaterThanUnitTest("1234", "123"); // should be true
-    singleGreaterThanUnitTest("45681565515", "45681566515"); // should be false
+    singleGreaterThanUnitTest("0", "0", false);
+    singleGreaterThanUnitTest("1", "0", true);
+    singleGreaterThanUnitTest("0", "1", false);
+    singleGreaterThanUnitTest("1234", "123", true);
+    singleGreaterThanUnitTest("45681565515", "45681566515", false);
 
-    singleGreaterThanUnitTest("1234", "-1234"); // should be true
-    singleGreaterThanUnitTest("-3456", "1234"); // should be false
-    singleGreaterThanUnitTest("-1234", "-3456"); // should be true
+    singleGreaterThanUnitTest("1234", "-1234", true);
+    singleGreaterThanUnitTest("-3456", "1234", false);
+    singleGreaterThanUnitTest("-1234", "-3456", true);
 }
 
 int main()
@@ -81,6 +117,11 @@ int main()
     subtractionUnitTests();
     lessThanUnitTests();
     greaterThanUnitTests();
+
+    std::cout << std::endl;
+    std::cout << numPassed << " passed" << std::endl;
+    std::cout << numFailed << " failed" << std::endl;
+    std::cout << std::endl;
 
     std::cout << "Press enter to continue..." << std::endl;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
